858c57dc07e2a387f85abd0c644d6acda4c50511
Bhargav200004 <Bhargav200004@gmail.com>
2023-12-25 02:21:12 +0530

    commit8

--- app/src/main/java/com/example/newszy/ui/components/headLine/HeadlineSection.kt
+++ app/src/main/java/com/example/newszy/ui/components/headLine/HeadlineSection.kt
@@ -1,7 +1,18 @@
 package com.example.newszy.ui.components.headLine
 
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
+import com.example.newszy.ui.screens.home.News
 
-fun LazyListScope.headLineSection(){
+fun LazyListScope.headLineSection(newsAll: List<News>) {
+    newsAll.forEach{news->
+        items(news.articles){
+
+        }
+    }
+
+
 
 }
+
+
--- app/src/main/java/com/example/newszy/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/newszy/ui/screens/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.example.newszy.ui.screens.home
 
-import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,10 +17,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.newszy.ui.components.headLine.headLineSection
 import com.example.newszy.ui.components.CategoryChip
 import com.example.newszy.ui.components.headLine.HeadLineTextSection
+import com.example.newszy.ui.components.headLine.headLineSection
 
+
+data class News(
+    val articles : List<Article>,
+    val status : String,
+    val totalResult : Int
+)
+
+data class Article(
+    val author: String,
+    val content: String,
+    val description: String,
+    val source : Source
+)
+
+data class Source(
+    val id: Int,
+    val name: String
+)
 @Composable
 fun HomeScreen() {
 
@@ -94,38 +114,112 @@ fun HomeScreen() {
     var selectedCountryText by remember { mutableStateOf("") }
 
 
+
+    val article = listOf(
+        Article(
+            author = "John Doe",
+            content = "Lorem ipsum content 1",
+            description = "Description 1",
+            source = Source(id = 1, name = "Source A")
+        ),
+        Article(
+            author = "Alice Smith",
+            content = "Lorem ipsum content 2",
+            description = "Description 2",
+            source = Source(id = 2, name = "Source B")
+        ),
+        Article(
+            author = "Bob Johnson",
+            content = "Lorem ipsum content 3",
+            description = "Description 3",
+            source = Source(id = 3, name = "Source C")
+        ),
+        Article(
+            author = "Eva Williams",
+            content = "Lorem ipsum content 4",
+            description = "Description 4",
+            source = Source(id = 4, name = "Source D")
+        ),
+        Article(
+            author = "Michael Brown",
+            content = "Lorem ipsum content 5",
+            description = "Description 5",
+            source = Source(id = 5, name = "Source E")
+        ),
+        Article(
+            author = "Sophia Davis",
+            content = "Lorem ipsum content 6",
+            description = "Description 6",
+            source = Source(id = 6, name = "Source F")
+        ),
+        Article(
+            author = "David Miller",
+            content = "Lorem ipsum content 7",
+            description = "Description 7",
+            source = Source(id = 7, name = "Source G")
+        ),
+        Article(
+            author = "Emma Wilson",
+            content = "Lorem ipsum content 8",
+            description = "Description 8",
+            source = Source(id = 8, name = "Source H")
+        ),
+        Article(
+            author = "Daniel Jackson",
+            content = "Lorem ipsum content 9",
+            description = "Description 9",
+            source = Source(id = 9, name = "Source I")
+        ),
+        Article(
+            author = "Olivia White",
+            content = "Lorem ipsum content 10",
+            description = "Description 10",
+            source = Source(id = 10, name = "Source J")
+        )
+    )
+
+    val news = listOf(
+        News(articles = article, status = "ok", totalResult = 20),
+        News(articles = article, status = "no", totalResult = 20),
+        News(articles = article, status = "ok", totalResult = 20),
+        News(articles = article, status = "no", totalResult = 20)
+    )
+
+
     Scaffold(
         topBar = {
             HomeScreenTopBar()
         }
     ) { paddingValues ->
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .padding(paddingValues = paddingValues)
                 .padding(10.dp)
         ) {
 
-            item {
-                // Category Section
-                CategoryChip(
-                    category = category,
-                    selectedCategory = selectedCategory,
-                    onSelectedCategoryChange = { selectedCategory = it}
-                )
+            // Category Section
+            CategoryChip(
+                category = category,
+                selectedCategory = selectedCategory,
+                onSelectedCategoryChange = { selectedCategory = it }
+            )
 
-                //HeadLine Text Section
-                HeadLineTextSection(
-                    countryNames = countryNames,
-                    selectedCountry = selectedCountryText,
-                    onSelectedCountryChange ={
-                        selectedCountryText = it
-                    }
-                )
+            //HeadLine Text Section
+            HeadLineTextSection(
+                countryNames = countryNames,
+                selectedCountry = selectedCountryText,
+                onSelectedCountryChange = {
+                    selectedCountryText = it
+                }
+            )
+
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                //HeadLine Section
+                headLineSection(news)
             }
-
-            //HeadLine Section
-            headLineSection()
-
             //Main Section
 
         }
