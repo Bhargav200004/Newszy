0b40ac95af90b756c1b5cfe379516f7b3b572a70
Bhargav200004 <Bhargav200004@gmail.com>
2024-01-07 01:23:25 +0530

    commit19 ( adding some app photos )

--- app/src/main/java/com/example/newszy/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newszy/domain/repository/NewsRepository.kt
@@ -3,7 +3,6 @@ package com.example.newszy.domain.repository
 import com.example.newszy.domain.model.NewsFetch
 
 interface NewsRepository{
-
     suspend fun getHeadlines(country : String , category : String) : NewsFetch
     suspend fun getEveryThing(q : String = "title") : NewsFetch
 }
